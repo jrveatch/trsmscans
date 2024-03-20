52b76fd0a2938453a22b7351765b2fb9258259db
Jason Veatch <jason.veatch@cern.ch>
2024-03-19 21:56:58 -0700

    One more script fix

--- first.sh
+++ first.sh
@@ -4,6 +4,7 @@ source scripts/setupsubmodules.sh
 
 # set up python virtual environment
 python3 -m venv trsm_venv
+source trsm_venv/bin/activate
 pip install -r python/requirements.txt
 
 # install ScannerS and HiggsTools
