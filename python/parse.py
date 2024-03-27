3420032da934b566ccc2d33f5604f10171d1554e
Jason Veatch <jason.veatch@cern.ch>
2024-03-26 22:39:41 -0700

    Apply filters in parse.py

--- python/parse.py
+++ python/parse.py
@@ -14,11 +14,18 @@ class Parse:
 
     # load new arrays
     def loadArrays(self,filename):
+        # load arrays from file
         self.arr.loadArrays(filename)
+        # get arrays masked by filters
+        self.getFilteredArrays()
 
     # get the arrays
     def getArrays(self):
         return self.arr
+
+    # get arrays of the filters
+    def getFilters(self):
+        self.filters = np.multiply(self.arr.data['filt_width'],self.arr.data['filt_bounds'])
 
     # find the point that maximizes xb
     def getmaxpoint(self,decay):
@@ -33,11 +40,11 @@ class Parse:
         maxxb = xb[maxidx]
 
         # get theta and vev values that maximize xsec times BR
-        maxthS = self.arr.data['thetahS'][maxidx]
-        maxthX = self.arr.data['thetahX'][maxidx]
-        maxtSX = self.arr.data['thetaSX'][maxidx]
-        maxvs = self.arr.data['vs'][maxidx]
-        maxvx = self.arr.data['vx'][maxidx]
+        maxthS = self.thetahS[maxidx]
+        maxthX = self.thetahX[maxidx]
+        maxtSX = self.thetaSX[maxidx]
+        maxvs = self.vs[maxidx]
+        maxvx = self.vx[maxidx]
 
         return maxxb, maxthS, maxthX, maxtSX, maxvs, maxvx
 
@@ -61,7 +68,7 @@ class Parse:
         # TODO: take decay as argument for other channels modes
 
         # get production cross section
-        xb_prod = np.multiply(self.arr.data['x_H3_gg'],self.arr.data['b_H3_H1H2'])
+        xb_prod = np.multiply(self.x_H3_gg,self.b_H3_H1H2)
 
         return xb_prod
 
@@ -70,36 +77,36 @@ class Parse:
 
         match decay:
             case "H2bbH1tautau":
-                xb_decay = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_bb'])
+                xb_decay = np.multiply(self.b_H1_tautau,self.b_H2_bb)
             case "H2tautauH1bb":
-                xb_decay = np.multiply(self.arr.data['b_H1_bb'],self.arr.data['b_H2_tautau'])
+                xb_decay = np.multiply(self.b_H1_bb,self.b_H2_tautau)
             case "H2H1bbtautau":
-                arr1 = np.multiply(self.arr.data['b_H1_bb'],self.arr.data['b_H2_tautau'])
-                arr2 = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_bb'])
+                arr1 = np.multiply(self.b_H1_bb,self.b_H2_tautau)
+                arr2 = np.multiply(self.b_H1_tautau,self.b_H2_bb)
                 xb_decay = np.add(arr1,arr2)
             case "H2WWH1tautau":
-                xb_decay = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_WW'])
+                xb_decay = np.multiply(self.b_H1_tautau,self.b_H2_WW)
             case "H2tautauH1WW":
-                xb_decay = np.multiply(self.arr.data['b_H2_tautau'],self.arr.data['b_H1_WW'])
+                xb_decay = np.multiply(self.b_H2_tautau,self.b_H1_WW)
             case "H2H1WWtautau":
-                arr1 = np.multiply(self.arr.data['b_H2_tautau'],self.arr.data['b_H1_WW'])
-                arr2 = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_WW'])
+                arr1 = np.multiply(self.b_H2_tautau,self.b_H1_WW)
+                arr2 = np.multiply(self.b_H1_tautau,self.b_H2_WW)
                 xb_decay = np.add(arr1,arr2)
             case "H2ZZH1tautau":
-                xb_decay = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_ZZ'])
+                xb_decay = np.multiply(self.b_H1_tautau,self.b_H2_ZZ)
             case "H2tautauH1ZZ":
-                xb_decay = np.multiply(self.arr.data['b_H2_tautau'],self.arr.data['b_H1_ZZ'])
+                xb_decay = np.multiply(self.b_H2_tautau,self.b_H1_ZZ)
             case "H2H1ZZtautau":
-                arr1 = np.multiply(self.arr.data['b_H2_tautau'],self.arr.data['b_H1_ZZ'])
-                arr2 = np.multiply(self.arr.data['b_H1_tautau'],self.arr.data['b_H2_ZZ'])
+                arr1 = np.multiply(self.b_H2_tautau,self.b_H1_ZZ)
+                arr2 = np.multiply(self.b_H1_tautau,self.b_H2_ZZ)
                 xb_decay = np.add(arr1,arr2)
             case "H2VVH1tautau":
-                xb_decay = np.multiply(self.arr.data['b_H1_tautau'],np.add(self.arr.data['b_H2_WW'],self.arr.data['b_H2_ZZ']))
+                xb_decay = np.multiply(self.b_H1_tautau,np.add(self.b_H2_WW,self.b_H2_ZZ))
             case "H2tautauH1VV":
-                xb_decay = np.multiply(self.arr.data['b_H2_tautau'],np.add(self.arr.data['b_H1_WW'],self.arr.data['b_H1_ZZ']))
+                xb_decay = np.multiply(self.b_H2_tautau,np.add(self.b_H1_WW,self.b_H1_ZZ))
             case "H2H1VVtautau":
-                arr1 = np.multiply(self.arr.data['b_H2_tautau'],np.add(self.arr.data['b_H1_WW'],self.arr.data['b_H1_ZZ']))
-                arr2 = np.multiply(self.arr.data['b_H1_tautau'],np.add(self.arr.data['b_H2_WW'],self.arr.data['b_H2_ZZ']))
+                arr1 = np.multiply(self.b_H2_tautau,np.add(self.b_H1_W,self.b_H1_ZZ))
+                arr2 = np.multiply(self.b_H1_tautau,np.add(self.b_H2_WW,self.b_H2_ZZ))
                 xb_decay = np.add(arr1,arr2)
             case _:
                 print("Unrecognized decay",decay)
@@ -110,20 +117,46 @@ class Parse:
 
     def getparams(self):
 
-        minthS = np.min(self.arr.data['thetahS'])
-        maxthS = np.max(self.arr.data['thetahS'])
-        minthX = np.min(self.arr.data['thetahX'])
-        maxthX = np.max(self.arr.data['thetahX'])
-        mintSX = np.min(self.arr.data['thetaSX'])
-        maxtSX = np.max(self.arr.data['thetaSX'])
+        minthS = np.min(self.thetahS)
+        maxthS = np.max(self.thetahS)
+        minthX = np.min(self.thetahX)
+        maxthX = np.max(self.thetahX)
+        mintSX = np.min(self.thetaSX)
+        maxtSX = np.max(self.thetaSX)
 
-        minvs = np.min(self.arr.data['vs'])
-        maxvs = np.max(self.arr.data['vs'])
-        minvx = np.min(self.arr.data['vx'])
-        maxvx = np.max(self.arr.data['vx'])
+        minvs = np.min(self.vs)
+        maxvs = np.max(self.vs)
+        minvx = np.min(self.vx)
+        maxvx = np.max(self.vx)
 
         return minthS, maxthS, minthX, maxthX, mintSX, maxtSX, minvs, maxvs, minvx, maxvx
     
     def getvars(self):
+        return self.thetahS, self.thetahX, self.thetaSX, self.vs, self.vx
+
+    # apply filters as mask
+    def getFilteredArrays(self):
         
-        return self.arr.data['thetahS'], self.arr.data['thetahX'], self.arr.data['thetaSX'], self.arr.data['vs'], self.arr.data['vx']
+        # get array of filters to use as a mask
+        self.getFilters()
+
+        # create local arrays by applying filter mask
+
+        # theta and vev values
+        self.thetahS = self.arr.data['thetahS'][self.filters != 0]
+        self.thetahX = self.arr.data['thetahX'][self.filters != 0]
+        self.thetaSX = self.arr.data['thetaSX'][self.filters != 0]
+        self.vs = self.arr.data['vs'][self.filters != 0]
+        self.vx = self.arr.data['vx'][self.filters != 0]
+
+        # xsec and br values
+        self.x_H3_gg = self.arr.data['x_H3_gg'][self.filters != 0]
+        self.b_H3_H1H2 = self.arr.data['b_H3_H1H2'][self.filters != 0]
+        self.b_H1_bb = self.arr.data['b_H1_bb'][self.filters != 0]
+        self.b_H1_tautau = self.arr.data['b_H1_tautau'][self.filters != 0]
+        self.b_H1_WW = self.arr.data['b_H1_WW'][self.filters != 0]
+        self.b_H1_ZZ = self.arr.data['b_H1_ZZ'][self.filters != 0]
+        self.b_H2_bb = self.arr.data['b_H2_bb'][self.filters != 0]
+        self.b_H2_tautau = self.arr.data['b_H2_tautau'][self.filters != 0]
+        self.b_H2_WW = self.arr.data['b_H2_WW'][self.filters != 0]
+        self.b_H2_ZZ = self.arr.data['b_H2_ZZ'][self.filters != 0]
