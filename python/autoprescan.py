ade3256a79c4c68fafb4a69efc2f5e3093b478c5
Jason Veatch <jason.veatch@cern.ch>
2024-03-30 15:14:48 -0700

    Adding autoprescan.py and masspoints.txt

---
+++ python/autoprescan.py
@@ -0,0 +1,57 @@
+
+import os
+import prescan
+import argparse
+
+def runAllPrescans(npoints,maxwidth,overwrite,use_multiprocessing):
+
+    # get the data directory
+    datadir = os.environ['DATADIR']
+
+    # file with list of mass points
+    masspointsfile = datadir+"/masspoints.txt"
+
+    with open(masspointsfile,"r") as masspoints:
+
+        # skip first line with headers
+        next(masspoints)
+
+        # read the rest of the file
+        for line in masspoints:
+
+            # parse the line and store the mass values
+            xmass, smass = line.strip().split()
+
+            # print info to screen
+            print("\n\nRunning prescan for XMass =",xmass,"SMass =",smass)
+
+            # run a prescan for each mass point
+            prescan.runPrescan(XMass=xmass,SMass=smass,npoints=npoints,
+                               maxwidth=maxwidth,overwrite=overwrite,
+                               use_multiprocessing=use_multiprocessing)
+
+if __name__ == "__main__":
+
+    # parse command line arguments
+    argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    argparser.add_argument("-n", "--npoints", default=50000, type=int, help="Initial number of scan points")
+    argparser.add_argument("-w", "--widthmax", default=0.15, type=float, help="Maximum allowed width for any scalar")
+    argparser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite previous prescan")
+    argparser.add_argument("-s", "--single_process", action="store_true", help="Use if multiprocessing should not be used")
+    args = vars(argparser.parse_args())
+
+    # number of points to run
+    npoints = args["npoints"]
+
+    # get maximum width
+    maxwidth = args["widthmax"]
+
+    # run options
+    overwrite = args["overwrite"]
+    use_single_process = args["single_process"]
+
+    # run all prescans
+    runAllPrescans(npoints=npoints,maxwidth=maxwidth,
+                   overwrite=overwrite,
+                   use_multiprocessing=not use_single_process)
+    
