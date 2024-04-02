4aa50e1644f8977e27c5eb153d6c55df53d8d001
Jason Veatch <jason.veatch@cern.ch>
2024-04-01 19:24:58 -0700

    Simplifying directories script

--- scripts/outputdirs.sh
+++ scripts/outputdirs.sh
@@ -1,17 +1,12 @@
 
 #!/bin/bash
 
-# make output directory if it doesn't exist
-if [ ! -d "run/output" ]; then
-    mkdir run/output
-fi
-
 # make prescan output directory if it doesn't exist
 if [ ! -d "run/output/prescan" ]; then
-    mkdir run/output/prescan
+    mkdir -p run/output/prescan
 fi
 
 # make scan output directory if it doesn't exist
 if [ ! -d "run/output/scan" ]; then
-    mkdir run/output/scan
+    mkdir -p run/output/scan
 fi
