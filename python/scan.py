5674ad7e25f20335fa42edf3a6e4cb56814c45f0
Jason Veatch <jason.veatch@cern.ch>
2024-04-01 23:36:44 -0700

    Moving to .txt file of available decay modes

--- python/scan.py
+++ python/scan.py
@@ -37,15 +37,7 @@ def runScan(XMass,
     HMass = 125
 
     # check to make sure decay mode is supported
-    supported = False
-    if decay == "H2bbH1tautau" or decay == "H2tautauH1bb" or decay == "H2H1bbtautau":
-        supported = True
-    if decay == "H2WWH1tautau" or decay == "H2tautauH1WW" or decay == "H2H1WWtautau":
-        supported = True
-    if decay == "H2ZZH1tautau" or decay == "H2tautauH1ZZ" or decay == "H2H1ZZtautau":
-        supported = True
-    if decay == "H2VVH1tautau" or decay == "H2tautauH1VV" or decay == "H2H1VVtautau":
-        supported = True
+    supported = isValidDecay(decay)
     if not supported:
         print("Unrecognized decay",decay)
         print("Quitting...")
@@ -468,6 +460,26 @@ def runScan(XMass,
     details = open(detailsname,"a")
     details.write("\nScan took "+str(datetime.timedelta(seconds=int(scantime)))+" (hh:mm:ss)")
     details.close()
+
+def isValidDecay(decaymode):
+
+    # decay mode file name
+    filename = os.environ['DATADIR'] + "decaymodes.txt"
+
+    # search for decaymode in file
+    with open(filename, 'r') as file:
+        # loop over every line in the file
+        for line in file:
+            # skip blank lines
+            if line.strip():
+                # get first word from each line
+                first_word = line.split()[0]
+                if first_word == decaymode:
+                    # if it is found, return True
+                    return True
+
+    # if it isn't found, return False
+    return False
 
 if __name__ == "__main__":
 
