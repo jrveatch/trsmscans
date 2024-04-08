d82dfcc264a5e90eda9d42be41610243db679459
Jason Veatch <jason.veatch@cern.ch>
2024-04-07 21:26:54 -0700

    Redoing float casting

--- python/bounds.py
+++ python/bounds.py
@@ -19,7 +19,7 @@ def filterbounds(filename,SMass,debug=False):
 
     # if mS > mH, switch order
     HgtS = False
-    if float(SMass) < 125:
+    if SMass < 125:
         SName = 'H2'
         HName = 'H1'
         HgtS = True
