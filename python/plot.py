1a692d65adb8bd4a46933bab9e926146a3fa0b95
dreamysom <rroxana.rodriguez23@gmail.com>
2024-04-10 16:57:59 -0700

    cleaning out plot.py

--- python/plot.py
+++ python/plot.py
@@ -1,41 +1,23 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-import columns
 import parse
 
-decay = "H2bbH1tautau"
-xmass = "500"
+decay = "SbbHtautau"
+xmass = "1000"
 smass = "300"
-niter = 16
+niter = 2
 
 prescan = "output/prescan/X"+xmass+"_S"+smass+"/TRSMBroken_prescan.tsv"
-# get list of column numbers
-cols = columns.Columns(prescan)
 
-for iter in range(niter):
-    identifier = f"{iter:04d}"
+filename = prescan
 
-    print(identifier)
+# get arrays object
+parser = parse.Parse(filename,HMass=125,SMass=float(smass))
 
-    if iter == 0:
-        filename = prescan
+thetahS, thetahX, thetaSX, vs, vx = parser.getvars()
 
-    else:
-        filename = "output/scan/"+decay+"/X"+xmass+"_S"+smass+"/files/TRSMBroken_"+identifier+"_BOUNDS.tsv"
-
-    # get arrays object
-    parser = parse.Parse(filename,cols)
-
-    thetahS, thetahX, thetaSX, vs, vx = parser.getvars()
-
-    xb = parser.getxb(decay)
-
-    color = str(0.8*(1 - iter/niter))
-
-    print(color)
-
-    plt.scatter(thetahS,thetahX,s=10,c=color)
+xb = parser.getxb(decay)
 
 plt.scatter(thetahS,thetahX,s=30,c="hotpink")
 
