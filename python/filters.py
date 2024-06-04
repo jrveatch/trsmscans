29f9a8c35f5b105b0f59d46b66ca4ef8e7d8382b
Jason Veatch <jason.veatch@cern.ch>
2024-06-04 08:46:55 -0700

    Improve arguments

--- python/filters.py
+++ python/filters.py
@@ -1,14 +1,12 @@
 #!/usr/bin/env python3
 
 import argparse
-
 import numpy as np
 
-from arrays import Arrays
 import width
 import bounds
 import tsvutils
-
+from arrays import Arrays
 from masses import Masses
 
 header_width = "filt_width"
@@ -56,6 +54,12 @@ if __name__ == "__main__":
     argparser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
     argparser.add_argument("-f", "--filename", help="Name of file to apply filters to")
     argparser.add_argument("-w", "--maxwidth", default=0.15, type=float, help="Maximum allowed width for any scalar")
+    argparser.add_argument("-X", "--XMass", required=True, type=float, help="Mass of scalar X in GeV")
+    argparser.add_argument("-S", "--SMass", required=True, type=float, help="Mass of scalar S in GeV")
+    argparser.add_argument("-H", "--HMass", default=125.09, type=float, help="Mass of scalar H in GeV")
     args = argparser.parse_args()
 
-    applyFilters(filename=args.filename,maxwidth=args.maxwidth)
+    # create masses
+    masses = Masses(mX=args.XMass,mS=args.SMass,mH=args.HMass)
+
+    applyFilters(filename=args.filename,maxwidth=args.maxwidth,masses=masses)
