5264964ba2effed57b979b1dd954f081068cc6d0
Steven Berdak <stevenberdak@gmail.com>
2024-06-06 17:59:10 -0700

    Fixed typo in mass size, added xb column to test data out file

--- python/steven_parse_test.py
+++ python/steven_parse_test.py
@@ -8,8 +8,11 @@ from parse import Parse
 
 ## Note: It would be a good idea to run 'sudo chmod 777 data' from the 'run' directory before attempting to run this file, or else the data file will not be written...
 
+mX=1001
+mS=301
+
 ROOT_PATH = os.path.dirname(__file__)
-TEST_FILE_REL = "../run/output/TRSMBroken/scan/SbbHtautau/X1001_S301/files/TRSMBroken_test_0000.tsv"
+TEST_FILE_REL = f'../run/output/TRSMBroken/scan/SbbHtautau/X{mX}_S{mS}/files/TRSMBroken_test_0000.tsv'
 
 # Using relative file here so that we can access it from any machine
 # os.path.dirname(__file__) gets the absolute path of the current file that we can join with our relative dir
@@ -17,13 +20,15 @@ file_path = os.path.abspath(os.path.join(ROOT_PATH, TEST_FILE_REL))
 
 print("file path = " + str(file_path))
 
-masses = Masses(mX=1000, mS=300, mH=125.09)
+masses = Masses(mX=mX, mS=mS, mH=125.09)
 parse = Parse(masses=masses, decay="SbbHtautau", modelname="TRSMBroken", filename=file_path)
 
 target = parse.getXB(decay="SbbHtautau")
 features = parse.getParameters()
+xb = parse.getXB()
 
 df = pd.DataFrame.from_dict(features)
+df.insert(len(df.columns), 'xb', xb)
 
 DATA_DIR_REL = "../run/data"
 out_path = os.path.abspath(os.path.join(ROOT_PATH, DATA_DIR_REL, "test.tsv"))
