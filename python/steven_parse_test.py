54b73e124f96ce58ce789e85147b26d1db559803
Steven Berdak <stevenberdak@gmail.com>
2024-06-06 17:40:43 -0700

    Added steven_parse_test.py file, begain work on file, generated tsv output to trsmscans/run/data dir

---
+++ python/steven_parse_test.py
@@ -0,0 +1,31 @@
+import importlib
+import os
+
+import pandas as pd
+
+from masses import Masses
+from parse import Parse
+
+## Note: It would be a good idea to run 'sudo chmod 777 data' from the 'run' directory before attempting to run this file, or else the data file will not be written...
+
+ROOT_PATH = os.path.dirname(__file__)
+TEST_FILE_REL = "../run/output/TRSMBroken/scan/SbbHtautau/X1001_S301/files/TRSMBroken_test_0000.tsv"
+
+# Using relative file here so that we can access it from any machine
+# os.path.dirname(__file__) gets the absolute path of the current file that we can join with our relative dir
+file_path = os.path.abspath(os.path.join(ROOT_PATH, TEST_FILE_REL))
+
+print("file path = " + str(file_path))
+
+masses = Masses(mX=1000, mS=300, mH=125.09)
+parse = Parse(masses=masses, decay="SbbHtautau", modelname="TRSMBroken", filename=file_path)
+
+target = parse.getXB(decay="SbbHtautau")
+features = parse.getParameters()
+
+df = pd.DataFrame.from_dict(features)
+
+DATA_DIR_REL = "../run/data"
+out_path = os.path.abspath(os.path.join(ROOT_PATH, DATA_DIR_REL, "test.tsv"))
+df.to_csv(out_path, sep="\t", index=False)
+print("File written...")
