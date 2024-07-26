f43d60ff968fa279c48db3b598c65dd27d914192
Jason Veatch <jason.veatch@cern.ch>
2024-07-25 17:25:55 -0700

    Fix logic error checking if files are the same

--- python/utils/tsvutils.py
+++ python/utils/tsvutils.py
@@ -27,12 +27,12 @@ def countPointsInTSV(filename):
 # function to save output tsv file
 def saveTSVOutput(inputfile,outputfile):
 
-    # Normalize paths to absolute paths for comparison
+    # normalize paths to absolute paths for comparison
     inputfile = os.path.abspath(inputfile)
     outputfile = os.path.abspath(outputfile)
     
-    # Check if inputfile and outputfile point to the same file
-    if os.path.samefile(inputfile, outputfile):
+    # check if inputfile and outputfile point to the same file
+    if inputfile==outputfile:
         print(f"Error: Input file '{inputfile}' and output file '{outputfile}' point to the same file.")
         return
 
