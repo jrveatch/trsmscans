fc077f8632248187c015793feb891ae7354e544e
Jason Veatch <jason.veatch@cern.ch>
2024-07-26 10:11:23 -0700

    Add particles to model yml

--- python/model.py
+++ python/model.py
@@ -27,9 +27,43 @@ class Model():
         if not os.path.isfile(self.templateini):
             raise FileNotFoundError("Template .ini file " + self.templateini + " does not exist. Exiting.")
 
+        self.readYaml()
+
+    # read .yml file
+    def readYaml(self):
+
         # read in model yaml file
         with open(self.ymlfile,'r') as file:
-            self.params = yaml.safe_load(file)[self.name]
+            # read yaml data for model
+            yaml_data = yaml.safe_load(file)[self.name]
+            # read particles
+            self.particles = yaml_data['particles']
+            # read parameters
+            self.params = yaml_data['parameters']
+
+        # convert NoneType entries to empty dictionaries
+        for key in self.particles:
+            if self.particles[key] == None:
+                self.particles[key] = {}
+        
+        # make sure exactly 1 SM-like Higgs is provided
+        if not len(self.particles['SMHiggs']) == 1:
+            print('1 SM Higgs expected, found ' + len(self.particles['SMHiggs']))
+            return
+        
+        # store SM-like Higgs
+        self.SMHiggs = self.particles['SMHiggs'][0]
+
+        # store BSM scalars
+        self.BSMScalars = []
+        for key in self.particles:
+            # skip SM-like Higgs for this list
+            if key == 'SMHiggs':
+                continue
+            self.BSMScalars.extend(self.particles[key])
+        
+        # store list of all scalars
+        self.AllScalars = self.particles['SMHiggs'] + self.BSMScalars
 
     # get list of parameter name
     def parameterList(self):
