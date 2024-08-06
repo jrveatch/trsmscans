18f4dc0234169dff53557d6984e7af2b1e566009
Jason Veatch <jason.veatch@cern.ch>
2024-08-05 22:35:11 -0700

    Move function to decayutils

--- python/scan.py
+++ python/scan.py
@@ -22,6 +22,7 @@ from utils.masses import Masses
 import prescan
 from utils import fileutils
 from utils import tsvutils
+from utils.decayutils import isValidDecay
 
 import copy
 import itertools 
@@ -374,26 +375,6 @@ class Scan:
 
         # Return list of all scanners
         return all_scanners
-
-def isValidDecay(decaymode: str) -> bool:
-
-    # decay mode file name
-    filename = os.environ['DATADIR'] + "decaymodes.txt"
-
-    # search for decaymode in file
-    with open(filename, 'r') as file:
-        # loop over every line in the file
-        for line in file:
-            # skip blank lines
-            if line.strip():
-                # get first word from each line
-                first_word = line.split()[0]
-                if first_word == decaymode:
-                    # if it is found, return True
-                    return True
-
-    # if it isn't found, return False
-    return False
 
 # class that keeps track of a single scan procedure
 class Scanner:
