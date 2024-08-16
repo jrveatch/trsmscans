99927e9d76bfdba88c3b6a4d5a854b6afddfc4bc
Jason Veatch <jason.veatch@cern.ch>
2024-08-15 19:41:27 -0700

    Remove incremental prescanning

--- python/prescan.py
+++ python/prescan.py
@@ -15,13 +15,13 @@ from utils import fileutils
 from utils.params import Params
 from utils.masses import Masses
 
+# TODO: Make this return a Parse object
 def run_prescan(masses: 'Masses',
                 modelname: str,
                 npoints: int,
                 maxwidth: float,
                 overwrite: bool = False,
-                use_multiprocessing: bool = False,
-                stepsize: int = 10000):
+                use_multiprocessing: bool = False) -> int:
 
     # get scan start time
     scanstart = time.time()
@@ -103,53 +103,27 @@ def run_prescan(masses: 'Masses',
         print(npointsOld,"prescan points requested, so I am running with",npoints,"points.")
         print("If you want to overwrite the existing prescan, run with -o.")
 
-    # increment up to the total number of points this will
-    # run the prescan to add stepsize points each time
-    # this approach takes a slightly longer time but allows
-    # progress to be captured at smaller increments in case
-    # the longer prescan runs get interrupted
+    # run ScannerS to sample points
+    result = runScannerS(ininame=ininame,
+                         modelname=modelname,
+                         npoints=npoints,
+                         use_multiprocessing=use_multiprocessing)
 
-    # number of points that have already been run
-    points_done = 0
+    # if a process returns a negative result, delete directory and return result
+    if result < 0:
 
-    # keep going while fewer than npoints have been done so far
-    while points_done < npoints:
+        # inform user
+        print("Removing directory",outdir)
 
-        # if there is space, run another set of stepsize
-        if npoints - points_done > stepsize:
-            points_to_run = stepsize
+        # delete directory
+        shutil.rmtree(outdir)
 
-        # otherwise run the remaining points
-        else:
-            points_to_run = npoints - points_done
+        # return result from process
+        return result
 
-        # run ScannerS for the next set of points
-        result = runScannerS(ininame=ininame,
-                             modelname=modelname,
-                             npoints=points_to_run,
-                             use_multiprocessing=use_multiprocessing)
-
-        # if a process returns a negative result, delete directory and return result
-        if result < 0:
-
-            # inform user
-            print("Removing directory",outdir)
-
-            # delete directory
-            shutil.rmtree(outdir)
-
-            # return result from process
-            return result
-
-        # increment the count of points done
-        points_done += tsvutils.count_tsv_points(tsvname_initial)
-
-        # initialize filter columns
-        initialize_filters(tsvname_initial)
-
-        # save output to tsvname
-        tsvutils.save_tsv_output(inputfile=tsvname_initial,
-                                 outputfile=tsvname)
+    # save output to tsvname
+    tsvutils.save_tsv_output(inputfile=tsvname_initial,
+                             outputfile=tsvname)
 
     # apply width and bounds filters
     apply_filters(filename=tsvname,
@@ -182,7 +156,6 @@ if __name__ == "__main__":
     argparser.add_argument("-w", "--maxwidth", default=0.15, type=float, help="Maximum allowed width for any scalar")
     argparser.add_argument("-o", "--overwrite", action="store_true", help="Overwrite previous prescan")
     argparser.add_argument("-m", "--multiprocessing", action="store_true", help="Use if multiprocessing should be used")
-    argparser.add_argument("-t", "--stepsize", default=10000, type=int, help="Step size to save progress")
     args = argparser.parse_args()
 
     # create masses object
@@ -193,5 +166,4 @@ if __name__ == "__main__":
                 npoints=args.npoints,
                 maxwidth=args.maxwidth,
                 overwrite=args.overwrite,
-                use_multiprocessing=args.multiprocessing,
-                stepsize=args.stepsize)
+                use_multiprocessing=args.multiprocessing)
