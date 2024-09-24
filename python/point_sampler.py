73464892af613cb5226fd28d4344f71a31356dd3
Jason Veatch <jason.veatch@cern.ch>
2024-09-24 13:37:17 -0700

    Minor cleanup on point sampler

--- python/point_sampler.py
+++ python/point_sampler.py
@@ -34,7 +34,7 @@ class PointSampler:
 
         #Global variable for number of points
         self.npoints = npoints
-            
+
         # write new .ini file from template and parameters
         params.write_ini(self.ininame)
 
@@ -54,33 +54,28 @@ class PointSampler:
         while self.npass < self.npoints:
 
             # Print number of points requested
-            print(f'\n{npoints} points requested')
+            print(f'{npoints} points requested')
 
             # Run ScannerS
             points = runScannerS(ini_name=self.ininame,
                                  num_points=npoints,
                                  model_name=self.model_name,
                                  use_multiprocessing=True)
-            
-            print(f'\n{points} points run')
 
             # Update the total points run
             self.curr_points_run += points
 
             # Print
-            print("\nApplying filters to the points...")
+            print("Applying filters...")
 
             # Apply width and bounds filters
             nwidth, nbounds, nsignals, npass = filters.filter.apply_filters(file_name=temptsv,
                                                                             masses=params.masses(),
                                                                             config_loader=self.config_loader)
-            
-            # Print points passed
-            print(f'{npass} points passed the filters', end=" ")
-            
+
             # Concatenate the infromation from temptsv to the tsv file
             tsvutils.save_tsv_output(temptsv, tsvname)
-            
+
             # Update the filtered variables
             self.npass += npass
             self.nbounds += nbounds
@@ -90,8 +85,8 @@ class PointSampler:
             # Calculate the efficency of the points passed based on points run
             efficiency = self.npass/self.curr_points_run
 
-            # Print efficiency
-            print(f'with an efficiency of {efficiency*100}%\n')
+            # Print points passed and efficiency
+            print(f'{npass} points passed the filters with an efficiency of {100*efficiency:.1f}%\n')
 
             # Add cushion to the efficiency
             efficiency *= 1.05
@@ -107,18 +102,18 @@ class PointSampler:
     #Return the variables
     def get_nwidth(self) -> float:
         return self.nwidth
-    
+
     def get_nbounds(self):
         return self.nbounds
-    
+
     def get_nsignals(self):
         return self.nsignals
-    
+
     def get_npass(self):
         return self.npass
-    
+
     def all_points_run(self):
         return self.curr_points_run
-    
+
 if __name__ == "__main__":
     pass
