d09c297d3137db383214e7d251a9daa9335fd0ba
Jason Veatch <jason.veatch@cern.ch>
2024-10-29 16:01:52 -0700

    Write tsv and ini files to correct directories

--- python/point_sampler.py
+++ python/point_sampler.py
@@ -14,7 +14,7 @@ from typing import List
 
 class PointSampler:
         
-    # Initializer, Passes output directory, model name, and config loader
+    # Initializer: passes output directory, model name, and config loader
     def __init__(self,
                  out_dir: str,
                  model_name: str,
@@ -22,16 +22,18 @@ class PointSampler:
                  config_loader: ConfigLoader,
                  use_file_dir: bool = False) -> None:
 
-        # Initialize Class Variables
+        # Initialize class variables
         self.out_dir = out_dir
-        self.file_dir = out_dir
+        self.ini_dir = out_dir
+        self.tsv_dir = out_dir
         if use_file_dir:
-            self.file_dir += "files/"
+            self.ini_dir += "files/ini/"
+            self.tsv_dir += "files/tsv/"
         self.model_name = model_name
         self.use_multiprocessing = use_multiprocessing
         self.config_loader = config_loader
 
-    # Method to check the sample points
+    # Method to sample a number of points
     def sample_points(self,
                       params: Params,
                       identifier: str,
@@ -39,16 +41,16 @@ class PointSampler:
                       good_points_only: bool = True) -> Parse:
 
         # set names of input .ini and output .tsv files
-        out_name = self.file_dir + self.model_name + "_" + identifier
-        self.ini_name = out_name + ".ini"
-        tsv_name = out_name + ".tsv"
+        out_name = self.model_name + "_" + identifier
+        ini_name = self.ini_dir + out_name + ".ini"
+        tsv_name = self.tsv_dir + out_name + ".tsv"
         temp_tsv = self.out_dir + self.model_name + ".tsv"
 
         #Global variable for number of points
         self.npoints = npoints
 
         # write new .ini file from template and parameters
-        params.write_ini(self.ini_name)
+        params.write_ini(ini_name)
 
         # Initialize parser
         self.parser = Parse(params.masses(), self.model_name)
@@ -69,7 +71,7 @@ class PointSampler:
             print(f'{npoints} points requested')
 
             # Run ScannerS
-            points = runScannerS(ini_name = self.ini_name,
+            points = runScannerS(ini_name = ini_name,
                                  num_points = npoints,
                                  model_name = self.model_name,
                                  use_multiprocessing = self.use_multiprocessing)
