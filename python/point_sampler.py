b79f7ee4d82c51b304289abe11bfd573f4ba2907
dreamysom <rroxana.rodriguez23@gmail.com>
2024-11-19 23:45:47 -0800

    WIP -- Planning to Pull from Main and add Debug Logger

--- python/point_sampler.py
+++ python/point_sampler.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 # import tools
-import filters.filter
 from parse import Parse
 from utils.params import Params
 import filters.filter
+from filters.filter import apply_filters
 from utils.runScannerS import runScannerS
-
 from utils.tsv_utils import save_tsv_output
 from utils.config_loader import ConfigLoader
+
+import math
 
 from typing import List
 
@@ -30,12 +31,13 @@ class PointSampler:
         self.model_name = model_name
         self.use_multiprocessing = use_multiprocessing
         self.config_loader = config_loader
+        self.efficiency = 1.0
 
     # Method to check the sample points
     def sample_points(self,
                       params: Params,
                       identifier: str,
-                      npoints: int,
+                      num_points_requested: int,
                       good_points_only: bool = True) -> Parse:
 
         # set names of input .ini and output .tsv files
@@ -45,7 +47,7 @@ class PointSampler:
         temp_tsv = self.out_dir + self.model_name + ".tsv"
 
         #Global variable for number of points
-        self.npoints = npoints
+        self.total_points_requested = num_points_requested
 
         # write new .ini file from template and parameters
         params.write_ini(self.ini_name)
@@ -63,14 +65,17 @@ class PointSampler:
         self.curr_points_run = 0
 
         # Run until points passed is >= points asked for
-        while self.npass < self.npoints:
+        while self.npass < self.total_points_requested:
+
+            # Calculate number of points needed for next iteration -- round up to nearest whole number
+            num_points_requested = math.ceil((self.total_points_requested-self.npass)/self.efficiency)
 
             # Print number of points requested
-            print(f'{npoints} points requested')
+            print(f'{num_points_requested} points requested')
 
             # Run ScannerS
             points = runScannerS(ini_name = self.ini_name,
-                                 num_points = npoints,
+                                 num_points = num_points_requested,
                                  model_name = self.model_name,
                                  use_multiprocessing = self.use_multiprocessing)
 
@@ -81,7 +86,7 @@ class PointSampler:
             print("Applying filters...")
 
             # Apply width and bounds filters
-            nwidth, nbounds, nsignals, npass = filters.filter.apply_filters(file_name=temp_tsv,
+            nwidth, nbounds, nsignals, npass = apply_filters(file_name=temp_tsv,
                                                                             masses=params.masses(),
                                                                             config_loader=self.config_loader)
 
@@ -101,14 +106,19 @@ class PointSampler:
             # Calculate the efficiency of the points passed based on points run
             efficiency = self.npass/self.curr_points_run
 
-            # Print points passed and efficiency
-            print(f'{npass} points passed the filters with an efficiency of {100*efficiency:.1f}%\n')
+            if abs((self.efficiency/efficiency)-1) > 0.05:
 
-            # Add cushion to the efficiency
-            efficiency *= 1.05
+                # Print points passed and efficiency
+                print(f'{npass} points passed the filters with an efficiency of {100*efficiency:.1f}%\n')
 
-            # Calculate number of points needed for next iteration -- round to nearest whole number
-            npoints = round((self.npoints-self.npass)/efficiency)
+                # Add cushion to the efficiency
+                efficiency *= 0.98
+
+                self.efficiency = efficiency
+
+            else:
+
+                print(f'{npass} points passed the filters with a previous efficiency of {100*self.efficiency*1.02:.1f}%\n') 
 
         # read output tsv into parser
         self.parser.read_file(file_name=tsv_name)
