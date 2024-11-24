75729d39de2226d03b7617dbc0876cc5b566bf49
Jason Veatch <jason.veatch@cern.ch>
2024-11-23 18:44:21 -0600

    Minor formatting and cleanup

--- python/utils/arrays.py
+++ python/utils/arrays.py
@@ -28,29 +28,33 @@ class Arrays:
     # load tsv columns into a pandas dataframe
     def load_arrays(self,
                     file_name: str = ""
-                    ) -> None:
+                   ) -> None:
 
         # if a new file name is provided, store it as class object
         if file_name:
             self.__file_name = file_name
 
         # load the file into a dataframe
-        self.dataframe = pd.read_csv(self.__file_name,sep='\t',header=0,usecols=lambda col: col != 'idx' and col != '')
+        self.dataframe = pd.read_csv(self.__file_name,
+                                     sep='\t',
+                                     header=0,
+                                     usecols=lambda col: col != 'idx' and col != '')
 
     # sample the dataframe to get a number of random rows
     def sample_rows(self,
-                    num_rows: int) -> pd.DataFrame:
+                    num_rows: int
+                   ) -> pd.DataFrame:
         return self.dataframe.sample(n=num_rows)
 
     # set the values of an array
     def set_array(self,
-                 column_name: str,
-                 new_values: pd.Series
-                 #new_array: NDArray
+                  column_name: str,
+                  new_values: pd.Series
                  ) -> None:
         self.dataframe[column_name] = new_values
 
     # write arrays to a new file
     def write_file(self,
-                   file_name: str) -> None:
+                   file_name: str
+                  ) -> None:
         self.dataframe.to_csv(file_name, sep='\t', index=True, index_label='idx')
