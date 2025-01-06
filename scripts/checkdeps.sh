4d811c5372bd4d44fb15bcbb7e993e6dd564af2b
Jason Veatch <Jason.Veatch@cern.ch>
2025-01-06 23:33:34 +0100

    Improve python3 version checking

--- scripts/checkdeps.sh
+++ scripts/checkdeps.sh
@@ -11,6 +11,13 @@ python3_minimum_minor=$(echo "$python3_minimum" | cut -d. -f2)
 cmake_minimum_major=$(echo "$cmake_minimum" | cut -d. -f1)
 cmake_minimum_minor=$(echo "$cmake_minimum" | cut -d. -f2)
 
+# Check to make sure a version of Python3 is installed
+if ! command -v python3 &> /dev/null
+then
+  printf "Python3 is not installed. Please install $python3_minimum or newer and try again\n"
+  return 1
+fi
+
 # Get the Python3 version
 python3_version=$(python3 --version 2>&1 | cut -d' ' -f2)
 
@@ -18,11 +25,10 @@ python3_version=$(python3 --version 2>&1 | cut -d' ' -f2)
 python3_major=$(echo "$python3_version" | cut -d. -f1)
 python3_minor=$(echo "$python3_version" | cut -d. -f2)
 
-# Check if Python version is less than 3.10
+# Check if Python version is less than minimum
 if [[ $python3_major -lt $python3_minimum_major || ($python3_major -eq $python3_minimum_major && $python3_minor -lt $python3_minimum_minor) ]]; then
-    printf "Error: Python3 version $python3_version is below $python3_minimum\n"
-    printf "Please install $python3_minimum and try again\n"
-    return 1
+    printf "Warning: Your default Python3 version $python3_version is below the minimum $python3_minimum\n"
+    printf "Warning: You may need to specify a separate installation when setting up the venv\n"
 fi
 
 # Get the CMake version
@@ -32,7 +38,7 @@ cmake_version=$(cmake --version 2>&1 | head -n 1 | cut -d' ' -f3)
 cmake_major=$(echo "$cmake_version" | cut -d. -f1)
 cmake_minor=$(echo "$cmake_version" | cut -d. -f2)
 
-# Check if Python version is less than 3.10
+# Check if CMake version is less than minimum
 if [[ $cmake_major -lt $cmake_minimum_major || ($cmake_major -eq $cmake_minimum_major && $cmake_minor -lt $cmake_minimum_minor) ]]; then
     printf "Error: CMake version $cmake_version is below $cmake_minimum\n"
     printf "Please install $cmake_minimum and try again\n"
