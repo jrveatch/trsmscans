72ff42d01e23b0e8a1eefaf1ecfb02d6bcabbae8
Jason Veatch <jason.veatch@cern.ch>
2025-01-22 07:51:48 -0800

    Move get_non_resolvable_decay to decay_utils

--- python/utils/decay_utils.py
+++ python/utils/decay_utils.py
@@ -3,6 +3,8 @@
 import os
 from functools import lru_cache
 
+# TODO: Move from a simple .txt file to a .yaml/json file for better structure and parsing
+
 @lru_cache(maxsize=None)
 def valid_decays() -> set[str]:
 
@@ -27,3 +29,24 @@ def valid_decays() -> set[str]:
 def is_valid_decay(decay_mode: str) -> bool:
 
     return decay_mode in valid_decays()
+
+def get_non_resolvable_decay(decay: str) -> str:
+    if "bbbb" in decay:
+        return "Xbbbb"
+    if "bb" in decay and "tautau" in decay:
+        return "Xbbtautau"
+    if "bb" in decay and "WW" in decay:
+        return "XbbWW"
+    if "bb" in decay and "ZZ" in decay:
+        return "XbbZZ"
+    if "bb" in decay and "VV" in decay:
+        return "XbbVV"
+    if "bb" in decay and "gamgam" in decay:
+        return "Xbbgamgam"
+    if "WW" in decay and "tautau" in decay:
+        return "XWWtautau"
+    if "ZZ" in decay and "tautau" in decay:
+        return "XZZtautau"
+    if "VV" in decay and "tautau" in decay:
+        return "XVVtautau"
+    return "None"
