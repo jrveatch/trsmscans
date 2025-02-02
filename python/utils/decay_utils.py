64aceff39f39380dda60b346915cfa09856e087e
Jason Veatch <jason.veatch@cern.ch>
2025-02-02 15:06:45 -0800

    Better allowed decay mode handling

--- python/utils/decay_utils.py
+++ python/utils/decay_utils.py
@@ -2,51 +2,60 @@
 
 import os
 from functools import lru_cache
+import yaml
 
-# TODO: Move from a simple .txt file to a .yaml/json file for better structure and parsing
+class DecayConfigManager:
+    def __init__(self):
+        file_name = os.path.join(os.environ['DATADIR'], "decay_modes.yml")
+
+        # Load the configuration file once during initialization
+        with open(file_name, 'r') as config_file:
+            config = yaml.safe_load(config_file)
+        
+        self.allowed_decay_modes = config.get("allowed_decay_modes", {})
+        # Create a reverse mapping from values to groups and their non-resolvable version
+        self.decay_to_group = {}
+        self.non_resolvable_map = {}
+        self.valid_decay_modes: list[str] = []
+
+        for group, details in self.allowed_decay_modes.items():
+            self.valid_decay_modes.extend(details["all_modes"])
+            for mode in details["all_modes"]:
+                self.decay_to_group[mode] = group
+            self.non_resolvable_map[group] = details.get("non_resolvable")
+
+    def is_decay_allowed(self, decay):
+        """
+        Check whether a given decay is allowed in any decay group.
+        """
+        return decay in self.valid_decay_modes
+
+    def get_non_resolvable_decay(self, decay):
+        """
+        Get the non-resolvable version of the decay group to which the given decay belongs.
+        """
+        group = self.decay_to_group.get(decay)
+        if not group:
+            raise ValueError(f"Decay '{decay}' not found in any decay group.")
+        
+        # Return the non-resolvable version for the decay group
+        return self.non_resolvable_map.get(group)
 
 @lru_cache(maxsize=None)
 def valid_decays() -> set[str]:
 
-    # decay mode file name
-    file_name = os.path.join(os.environ['DATADIR'], "decaymodes.txt")
+    decay_config_manager = DecayConfigManager()
 
-    # initialize empty set for the available decay modes
-    strings_set = set()
-
-    # open the file
-    with open(file_name, "r") as file:
-        for line in file:
-            # strip leading/trailing whitespace
-            stripped_line = line.strip()
-            # skip comments and blank lines
-            if not stripped_line or stripped_line.startswith("#"):
-                continue
-            # add the valid string to the set
-            strings_set.add(stripped_line)
-    return strings_set
+    return decay_config_manager.valid_decay_modes
 
 def is_valid_decay(decay_mode: str) -> bool:
 
-    return decay_mode in valid_decays()
+    decay_config_manager = DecayConfigManager()
+
+    return decay_config_manager.is_decay_allowed(decay_mode)
 
 def get_non_resolvable_decay(decay: str) -> str:
-    if "bbbb" in decay:
-        return "Xbbbb"
-    if "bb" in decay and "tautau" in decay:
-        return "Xbbtautau"
-    if "bb" in decay and "WW" in decay:
-        return "XbbWW"
-    if "bb" in decay and "ZZ" in decay:
-        return "XbbZZ"
-    if "bb" in decay and "VV" in decay:
-        return "XbbVV"
-    if "bb" in decay and "gamgam" in decay:
-        return "Xbbgamgam"
-    if "WW" in decay and "tautau" in decay:
-        return "XWWtautau"
-    if "ZZ" in decay and "tautau" in decay:
-        return "XZZtautau"
-    if "VV" in decay and "tautau" in decay:
-        return "XVVtautau"
-    return "None"
+
+    decay_config_manager = DecayConfigManager()
+
+    return decay_config_manager.get_non_resolvable_decay(decay)
