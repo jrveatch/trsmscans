0e70e5636f11f9f0f7a8f8158532b6b69c89fab9
Jason Veatch <jason.veatch@cern.ch>
2025-02-12 12:57:06 -0800

    First attempt at new setup_submodules.sh

--- scripts/setup_submodules.sh
+++ scripts/setup_submodules.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/env sh
 
-git submodule init
-git submodule update
+set -e
+set -u
+set -o pipefail
+
+# Set environment variables from env.sh if it exists
+if [ -f env.sh ]; then
+    source env.sh
+fi
+
+# Check if SCANNERS_PATH is set, if so, skip ScannerS submodule
+if [ -z "${SCANNERS_PATH:-}" ]; then
+    echo "Initializing and updating ScannerS submodule..."
+    git submodule update --init --recursive ScannerS
+else
+    echo "Skipping ScannerS submodule as SCANNERS_PATH is set to $SCANNERS_PATH"
+fi
+
+# Check if HIGGSTOOLS_PATH is set, if so, skip higgstools submodule
+if [ -z "${HIGGSTOOLS_PATH:-}" ]; then
+    echo "Initializing and updating higgstools submodule..."
+    git submodule update --init --recursive higgstools
+else
+    echo "Skipping higgstools submodule as HIGGSTOOLS_PATH is set to $HIGGSTOOLS_PATH"
+fi
