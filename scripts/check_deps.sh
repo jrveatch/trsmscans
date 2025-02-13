18406b2d4033381b63918fa2546db5589072755c
Jason Veatch <jason.veatch@cern.ch>
2025-02-13 10:43:42 -0800

    Improved python and cmake version comparisons

--- scripts/check_deps.sh
+++ scripts/check_deps.sh
@@ -8,11 +8,19 @@ set -o pipefail
 python3_minimum="3.8"
 cmake_minimum="3.17"
 
-# Minimum majors and minors
-python3_minimum_major=$(echo "$python3_minimum" | cut -d. -f1)
-python3_minimum_minor=$(echo "$python3_minimum" | cut -d. -f2)
-cmake_minimum_major=$(echo "$cmake_minimum" | cut -d. -f1)
-cmake_minimum_minor=$(echo "$cmake_minimum" | cut -d. -f2)
+# Function to check if an installed version is less than the required version
+version_less_than() {
+  local installed_version=$1
+  local required_version=$2
+
+  local installed_major=$(echo "$installed_version" | awk -F. '{print $1}')
+  local installed_minor=$(echo "$installed_version" | awk -F. '{print $2}')
+  local required_major=$(echo "$required_version" | awk -F. '{print $1}')
+  local required_minor=$(echo "$required_version" | awk -F. '{print $2}')
+
+  [[ $installed_major -lt $required_major ]] || 
+  { [[ $installed_major -eq $required_major ]] && [[ $installed_minor -lt $required_minor ]]; }
+}
 
 ###########################################
 # Python
@@ -26,17 +34,15 @@ then
 fi
 
 # Get the Python3 version
-python3_version=$(python3 --version 2>&1 | cut -d' ' -f2)
-
-# Extract major and minor version numbers
-python3_major=$(echo "$python3_version" | cut -d. -f1)
-python3_minor=$(echo "$python3_version" | cut -d. -f2)
+python3_version=$(python3 --version 2>&1 | awk '{print $2}')
 
 # Check if Python version is less than minimum
-if [[ $python3_major -lt $python3_minimum_major || ($python3_major -eq $python3_minimum_major && $python3_minor -lt $python3_minimum_minor) ]]; then
+if version_less_than "$python3_version" "$python3_minimum"; then
     printf "Warning: Your default Python3 version $python3_version is below the minimum $python3_minimum\n"
     printf "Warning: You may need to specify a separate installation when setting up the venv\n"
 fi
+
+printf "Python version $python3_version is installed\n" 
 
 ###########################################
 # CMake
@@ -49,14 +55,10 @@ if ! command -v cmake &> /dev/null; then
 fi
 
 # Get the CMake version
-cmake_version=$(cmake --version 2>&1 | head -n 1 | cut -d' ' -f3)
-
-# Extract major and minor version numbers
-cmake_major=$(echo "$cmake_version" | cut -d. -f1)
-cmake_minor=$(echo "$cmake_version" | cut -d. -f2)
+cmake_version=$(cmake --version 2>&1 | awk 'NR==1 {print $3}')
 
 # Check if CMake version is less than minimum
-if [[ $cmake_major -lt $cmake_minimum_major || ($cmake_major -eq $cmake_minimum_major && $cmake_minor -lt $cmake_minimum_minor) ]]; then
+if version_less_than "$cmake_version" "$cmake_minimum"; then
     printf "Error: CMake version $cmake_version is below $cmake_minimum\n"
     printf "Please install $cmake_minimum and try again\n"
     exit 1
