7cdc57e675761f70cc460c1075441af368636e5f
Jason Veatch <jason.veatch@cern.ch>
2025-02-12 17:22:44 -0800

    Call setup_paths.sh

--- first.sh
+++ first.sh
@@ -7,19 +7,23 @@ if [ $ret -ne 0 ]; then
     return 1
 fi
 
+# set user paths
+printf "Setting up user paths\n"
+source scripts/setup_paths.sh
+
 # set up submodules
-echo "Setting up submodules"
-source scripts/setup_submodules.sh
+printf "Setting up submodules\n"
+./scripts/setup_submodules.sh
 
 # set up python virtual environment if it doesn't exist
 if [ ! -d trsm_venv ]; then
-    echo "Setting up python virtual environment"
-    source scripts/setup_venv.sh
+    printf "Setting up python virtual environment\n"
+    ./scripts/setup_venv.sh
 fi
 
 # source setup.sh to make sure all vars are set
 source setup.sh
 
 # install ScannerS and HiggsTools
-echo "Compiling ScannerS and higgstools"
+printf "Compiling ScannerS and higgstools\n"
 source scripts/compile.sh
