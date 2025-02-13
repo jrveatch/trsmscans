d481d2c3e73345833b2ab33a8ef9ae546376f933
Jason Veatch <jason.veatch@cern.ch>
2025-02-12 22:43:00 -0800

    Improve start_venv.sh

--- scripts/start_venv.sh
+++ scripts/start_venv.sh
@@ -1,4 +1,8 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
+
+set -e
+set -u
+set -o pipefail
 
 # start the python virtual environment
 printf "Activating venv\n"
@@ -7,10 +11,11 @@ source trsm_venv/bin/activate
 # check whether requirements file exists
 if ! [ -f "python/requirements.txt" ]; then
     printf "python/requirements.txt not found\n"
-    return
+    return 1
 fi
 
 # try to update all packages and only print the lines that are not already satisfied
 printf "Updating packages...\n"
-pip install -r "python/requirements.txt" | grep -v 'Requirement already satisfied'
+#pip install -r "python/requirements.txt" | grep -v 'Requirement already satisfied'
+pip install -r "python/requirements.txt" | grep -v 'Requirement already satisfied' || true
 printf "Packages are all up to date\n"
