9b8f5f1f436476b864056a8824a458de97a7ace8
Jason Veatch <jason.veatch@cern.ch>
2025-02-16 23:43:45 -0800

    Fix submodule paths

--- scripts/setup_submodules.sh
+++ scripts/setup_submodules.sh
@@ -12,30 +12,30 @@ fi
 # Check if SCANNERS_PATH is set, if so, skip ScannerS submodule
 if [ -z "${SCANNERS_PATH:-}" ]; then
     echo "Initializing and updating ScannerS submodule..."
-    git submodule update --init --recursive ScannerS
+    git submodule update --init --recursive externals/ScannerS
 else
     echo "Skipping ScannerS submodule as SCANNERS_PATH is set to $SCANNERS_PATH"
 fi
 
-# Check if HIGGSTOOLS_PATH is set, if so, skip higgstools submodule
+# Check if HIGGSTOOLS_PATH is set, if so, skip HiggsTools submodule
 if [ -z "${HIGGSTOOLS_PATH:-}" ]; then
-    echo "Initializing and updating higgstools submodule..."
-    git submodule update --init --recursive higgstools
+    echo "Initializing and updating HiggsTools submodule..."
+    git submodule update --init --recursive externals/higgstools
 else
     echo "Skipping higgstools submodule as HIGGSTOOLS_PATH is set to $HIGGSTOOLS_PATH"
 fi
 
-# Check if HBDATASET_PATH is set, if so, skip data/hbdataset submodule
+# Check if HBDATASET_PATH is set, if so, skip HBDataSet submodule
 if [ -z "${HBDATASET_PATH:-}" ]; then
-    echo "Initializing and updating data/hbdataset submodule..."
+    echo "Initializing and updating HBDataSet submodule..."
     git submodule update --init --recursive data/hbdataset
 else
     echo "Skipping data/hbdataset submodule as HBDATASET_PATH is set to $HBDATASET_PATH"
 fi
 
-# Check if HSDATASET_PATH is set, if so, skip data/hsdataset submodule
+# Check if HSDATASET_PATH is set, if so, skip HSDataSet submodule
 if [ -z "${HSDATASET_PATH:-}" ]; then
-    echo "Initializing and updating data/hsdataset submodule..."
+    echo "Initializing and updating HSDataSet submodule..."
     git submodule update --init --recursive data/hsdataset
 else
     echo "Skipping data/hsdataset submodule as HSDATASET_PATH is set to $HSDATASET_PATH"
