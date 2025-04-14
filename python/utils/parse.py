c03e4ea73347fd1cb9af21ca4055493be1ecce36
Jason Veatch <jason.veatch@cern.ch>
2025-04-13 19:03:51 -0700

    Moving to more properties in parse.py

--- python/utils/parse.py
+++ python/utils/parse.py
@@ -31,19 +31,18 @@ class Parse:
         self.__HName = model.get_ordered_scalar_name('H')
         self.__SName = model.get_ordered_scalar_name('S')
 
-        # initialize dictionaries of parameter arrays
-        self.__in_par_arrays: Dict[str,pd.Series] = {}
-        self.__out_par_arrays: Dict[str,pd.Series] = {}
-        self.__width_par_arrays: Dict[str,pd.Series] = {}
-        self.__par_arrays: Dict[str,pd.Series] = {}
-
         # get arrays from file name if it is provided
         if file_name:
             self.read_file(file_name)
 
     @property
+    def model(self) -> 'Model':
+        """Model object"""
+        return self.__model
+
+    @property
     def filtered_data(self) -> pd.DataFrame:
-        return self.data[self.__filters]
+        return self.data[self.filters]
 
     @property
     def tsv_header(self) -> str:
@@ -52,8 +51,28 @@ class Parse:
 
     @property
     def input_parameter_arrays(self) -> Dict[str,pd.Series]:
-        """Dictionary of the parameter arrays"""
-        return self.__in_par_arrays
+        """Dictionary of input parameter arrays"""
+        return {name: self.filtered_data[par['fullname']] for name, par in self.model.input_parameters.items()}
+
+    @property
+    def output_parameter_arrays(self) -> Dict[str,pd.Series]:
+        """Dictionary of output parameter arrays"""
+        return {name: self.filtered_data[par['fullname']] for name, par in self.model.output_parameters.items()}
+
+    @property
+    def width_parameter_arrays(self) -> Dict[str,pd.Series]:
+        """Dictionary of width parameter arrays"""
+        return {name: self.filtered_data[par['fullname']] for name, par in self.model.width_parameters.items()}
+
+    @property
+    def parameter_arrays(self) -> Dict[str,pd.Series]:
+        """Dictionary of parameter arrays"""
+        return {**self.input_parameter_arrays, **self.output_parameter_arrays, **self.width_parameter_arrays}
+
+    @property
+    def filters(self) -> pd.Series:
+        """Filter decisions as a boolean array"""
+        return (self.data['filt_width'] * self.data['filt_bounds'] * self.data['filt_signals']).astype(bool)
 
     @property
     def num_filtered_points(self) -> int:
@@ -73,9 +92,6 @@ class Parse:
         if not hasattr(self,"data"):
             self.data = get_df(file_name)
 
-        # get arrays masked by filters
-        self.__make_filtered_arrays()
-
     def get_max_xb_point(self,
                          decay: str) -> Point:
         """Find the point with the highest xb"""
@@ -87,22 +103,22 @@ class Parse:
         self.max_idx = xb.idxmax()
         
         # make dictionary for parameter values for max_xb
-        max_xb_par_vals = {par: array[self.max_idx] for par, array in self.__par_arrays.items()}
+        max_xb_par_vals = {par: array[self.max_idx] for par, array in self.parameter_arrays.items()}
 
         # return a point object holding xb and other parameters
         return Point(xb = xb[self.max_idx],
-                     model = self.__model,
+                     model = self.model,
                      par_vals = max_xb_par_vals)
 
     def get_min(self,
                 par_name: str) -> float:
         """Get the minimum value for a parameter in the data"""
-        return self.__par_arrays[par_name].min()
+        return self.parameter_arrays[par_name].min()
 
     def get_max(self,
                 par_name: str) -> float:
         """Get the maximum value for a parameter in the data"""
-        return self.__par_arrays[par_name].max()
+        return self.parameter_arrays[par_name].max()
 
     def is_bimodal(self,
                    param_name: str,
@@ -133,7 +149,7 @@ class Parse:
         threshold_value = xb.quantile(percentile_threshold)
 
         # get set of parameter values with xb in selected percentile
-        param_selected = self.__in_par_arrays[param_name][xb > threshold_value] 
+        param_selected = self.input_parameter_arrays[param_name][xb > threshold_value] 
 
         # use Hartigan's dip test for unimodality
         _, pval = diptest.diptest(param_selected)
@@ -375,28 +391,6 @@ class Parse:
 
         # return the decay BR
         return br_decay
-
-    def __set_filters(self) -> None:
-        """Get arrays of filter decisions"""
-        self.__filters = (self.data['filt_width'] * self.data['filt_bounds'] * self.data['filt_signals']).astype(bool)
-
-    def __make_filtered_arrays(self) -> None:
-        """Apply filter decisions as a mask"""
-        
-        # get array of filters to use as a mask
-        self.__set_filters()
-
-        # populate a dictionary of series for each input parameter
-        self.__in_par_arrays = {name: self.filtered_data[par['fullname']] for name, par in self.__model.input_parameters.items()}
-
-        # populate a dictionary of series for each output parameter
-        self.__out_par_arrays = {name: self.filtered_data[par['fullname']] for name, par in self.__model.output_parameters.items()}
-
-        # populate a dictionary of series for each width parameter
-        self.__width_par_arrays = {name: self.filtered_data[par['fullname']] for name, par in self.__model.width_parameters.items()}
-
-        # combine all parameter arrays
-        self.__par_arrays = {**self.__in_par_arrays, **self.__out_par_arrays, **self.__width_par_arrays}
 
     def write_max_xb_line(self,
                           file_name: str
