ac730f65127e1b3e8b6259790424b548f64b6912
Jason Veatch <jason.veatch@cern.ch>
2025-05-20 22:08:27 -0700

    Type hints and docstrings

--- python/utils/decay_utils.py
+++ python/utils/decay_utils.py
@@ -2,7 +2,7 @@
 
 import os
 from functools import lru_cache
-from typing import List, Set
+from typing import Dict, List, Set
 import yaml
 
 # local modules
@@ -16,9 +16,9 @@ class DecayConfigManager:
         with open(file_name, 'r') as config_file:
             config = yaml.safe_load(config_file)
 
-        self.allowed_decay_modes = config.get("allowed_decay_modes", {})
+        self.allowed_decay_modes: Dict[str, Dict[str, List[str]]] = config.get("allowed_decay_modes", {})
         # Create a reverse mapping from values to groups and their non-resolvable version
-        self.decay_to_group = {}
+        self.decay_to_group: Dict[str,str] = {}
         self.non_resolvable_map = {}
         self.valid_decay_modes: List[str] = []
 
@@ -28,15 +28,23 @@ class DecayConfigManager:
                 self.decay_to_group[mode] = group
             self.non_resolvable_map[group] = details.get("non_resolvable")
 
-    def is_decay_allowed(self, decay):
+    def is_decay_allowed(self, decay) -> bool:
         """
         Check whether a given decay is allowed in any decay group.
+
+        Args:
+            decay (str): The decay mode to check.
+        Returns:
+            bool: True if the decay mode is allowed, False otherwise.
         """
         return decay in self.valid_decay_modes
 
     def get_non_resolvable_decay(self, decay):
         """
         Get the non-resolvable version of the decay group to which the given decay belongs.
+
+        Args:
+            decay (str): The decay mode to check.
         """
         group = self.decay_to_group.get(decay)
         if not group:
@@ -53,6 +61,13 @@ def valid_decays() -> Set[str]:
     return decay_config_manager.valid_decay_modes
 
 def is_valid_decay(decay_mode: str) -> bool:
+    """
+    Check if a decay mode is valid.
+    Args:
+        decay_mode (str): The decay mode to check.
+    Returns:
+        bool: True if the decay mode is valid, False otherwise.
+    """
 
     decay_config_manager = DecayConfigManager()
 
