b7e8b45e1bf281b3be37777748960dfc2a65b394
Jason Veatch <jason.veatch@cern.ch>
2025-06-06 16:39:09 +0200

    Add quotation marks for PATH export in env.sh

--- scripts/set_user_paths.sh
+++ scripts/set_user_paths.sh
@@ -186,7 +186,7 @@ fi
 
 # Append the new PATH setting to env.sh
 echo 'if [[ ":$PATH:" != *":'"$scanners_bin_path"':"* ]]; then' >> env.sh
-echo '    export PATH='"$scanners_bin_path"':$PATH' >> env.sh
+echo '    export PATH="'"$scanners_bin_path"':$PATH"' >> env.sh
 echo 'fi' >> env.sh
 
 # Create file to indicate that all paths have been successfully set
