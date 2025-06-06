8ca2eb39983c99110a7a3ae236e2722bc0c8224a
Jason Veatch <jason.veatch@cern.ch>
2025-06-06 17:56:48 +0200

    Add function to delete PATH block from env.sh

--- scripts/set_user_paths.sh
+++ scripts/set_user_paths.sh
@@ -164,12 +164,14 @@ submodule_path HSDATASET_PATH "HSDataSet"
 # Call function to handle symlink creation for run/output
 create_output_symlink
 
-# Remove various directories and PATH from env.sh
+# Remove directory variables from env.sh
 remove_var_from_env "DATADIR"
 remove_var_from_env "EXTERNALSDIR"
 remove_var_from_env "CONFIGDIR"
 remove_var_from_env "OUTPUTDIR"
-remove_var_from_env "PATH"
+
+# Remove PATH variables from env.sh
+remove_path_blocks_from_env
 
 # Add various directories to env.sh
 echo "export DATADIR=\"${PWD}/data/\"" >> env.sh
