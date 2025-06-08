cbdbdf189d367397561111afb6677a731d32de69
Jason Veatch <jason.veatch@cern.ch>
2025-06-08 07:30:51 +0200

    Save venv path in env.sh

--- scripts/setup_venv.sh
+++ scripts/setup_venv.sh
@@ -7,4 +7,8 @@ fi
 
 # create the virtual environment
 $PYTHON3_EXE -m venv trsm_venv --upgrade-deps
+
+# add the virtual environment to env.sh
+echo "export TRSM_VENV_PATH=\"$(pwd)/trsm_venv\"" >> env.sh
+
 printf "Virtual environment trsm_venv successfully created\n"
