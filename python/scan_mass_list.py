2b6d3cc2496627c9334f717bea4d8c669547aab8
Jason Veatch <jason.veatch@cern.ch>
2025-06-09 18:16:09 +0200

    Add toggle between local and batch running

--- python/scan_mass_list.py
+++ python/scan_mass_list.py
@@ -8,9 +8,11 @@ from utils.mass_permutations import get_mass_permutations
 
 def scan_mass_list(model: str,
                    decay: str,
-                   identifier: str) -> None:
+                   identifier: str,
+                   batch_mode: bool) -> None:
     """
-    Processes directories based on permutations of two values, includes headers, and writes the last line of .tsv files to a new .tsv file.
+    Scan a list of mass points based on permutations of two values.
+    By default, it runs locally, but can be usd for HTCondor submission.
 
     Args:
         model (str): Name of the theoretical model.
@@ -19,6 +21,8 @@ def scan_mass_list(model: str,
     """
 
     permutations = get_mass_permutations(decay=decay, identifier=identifier)
+
+    command = "scan_htcondor.py" if batch_mode else "scan.py"
 
     for XMass, SMass, resolvable in permutations:
         decay_mode = decay
@@ -34,7 +38,7 @@ def scan_mass_list(model: str,
             "-s", "zoom",
             "-n", "10000"
         ]
-        result = subprocess.run(["scan.py", *arg_list], text=True)
+        result = subprocess.run([command, *arg_list], text=True)
         print(f"{result}\n")
 
 if __name__ == "__main__":
@@ -44,8 +48,10 @@ if __name__ == "__main__":
     arg_parser.add_argument("-m", "--model", required=True, type=str, help="Model name")
     arg_parser.add_argument("-d", "--decay", required=True, type=str, help="Decay mode")
     arg_parser.add_argument("-i", "--identifier", required=True, type=str, help="Set identifier")
+    arg_parser.add_argument("-b", "--batch", action="store_true", help="Use batch mode for HTCondor submission")
     args = arg_parser.parse_args()
 
     scan_mass_list(model=args.model,
                    decay=args.decay,
-                   identifier=args.identifier)
+                   identifier=args.identifier,
+                   batch_mode=args.batch)
