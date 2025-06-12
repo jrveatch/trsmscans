0322782e58a09960270094653a2592c4ddee9529
Jason Veatch <jveatch@cern.ch>
2025-06-12 15:23:45 +0200

    Add ability to prescan mass grid on htcondor (#400)
    Co-authored-by: Jason Veatch <jason.veatch@cern.ch>

--- python/prescan_mass_list.py
+++ python/prescan_mass_list.py
@@ -7,17 +7,24 @@ from utils.mass_json_utils import get_mass_permutations
 
 def scan_mass_list(model: str,
                    decay: str,
-                   identifier: str) -> None:
+                   identifier: str,
+                   batch_mode: bool) -> None:
     """
-    Processes directories based on permutations of two values, includes headers, and writes the last line of .tsv files to a new .tsv file.
+    Prescan a list of mass points based on permutations of two values.
+    By default, it runs locally, but can be used for HTCondor submission.
 
     Args:
         model (str): Name of the theoretical model.
         decay (str): Decay mode.
         identifier (str): Identifier to specify which set of mass points to use.
+        batch_mode (bool): Whether to submit jobs using HTCondor.
     """
 
     permutations = get_mass_permutations(decay=decay, identifier=identifier)
+
+    command = "prescan_htcondor.py" if batch_mode else "prescan.py"
+
+    job_count = 0
 
     for XMass, SMass, _ in permutations:
         arg_list = [
@@ -26,9 +33,10 @@ def scan_mass_list(model: str,
             "-S", str(SMass),
             "-n", "10000"
         ]
-        subprocess.run(["prescan.py", *arg_list], text=True)
+        subprocess.run([command, *arg_list], text=True)
+        job_count += 1
 
-        # TODO: Add htcondor submission if needed
+    print(f"Submitted {job_count} jobs.")
 
 if __name__ == "__main__":
 
@@ -37,8 +45,10 @@ if __name__ == "__main__":
     arg_parser.add_argument("-m", "--model", required=True, type=str, help="Model name")
     arg_parser.add_argument("-d", "--decay", required=True, type=str, help="Decay mode")
     arg_parser.add_argument("-i", "--identifier", required=True, type=str, help="Set identifier")
+    arg_parser.add_argument("-b", "--batch", action="store_true", help="Use batch mode for HTCondor submission")
     args = arg_parser.parse_args()
 
     scan_mass_list(model=args.model,
                    decay=args.decay,
-                   identifier=args.identifier)
+                   identifier=args.identifier,
+                   batch_mode=args.batch)
