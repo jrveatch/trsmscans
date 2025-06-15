e756bfba2751555edb948b4bb60dd2452c4f2382
Jason Veatch <jason.veatch@cern.ch>
2025-06-16 00:01:34 +0200

    Whoops

--- python/utils/exceptions.py
+++ python/utils/exceptions.py
@@ -1,4 +1,11 @@
 
+from typing import Optional
+from logging import Logger
+
 class NoPointsPassedError(Exception):
-    def __init__(self, message="No points passed the filters."):
+    def __init__(self,
+                 message="No points passed the filters.",
+                 logger: Optional[Logger] = None):
+        if logger:
+            logger.error(message)
         super().__init__(message)
