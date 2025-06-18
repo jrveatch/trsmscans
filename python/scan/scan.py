23dffa80b97704265b8fc03100879ee47f91dfb6
Jason Veatch <jason.veatch@cern.ch>
2025-06-18 19:00:14 +0200

    Distribute points based on effective volume

--- python/scan/scan.py
+++ python/scan/scan.py
@@ -548,7 +548,7 @@ class Scan:
         num_param_spaces = len(param_space_list)
 
         # Initialize list of param space volumes
-        volumes = np.array([space.volume() for space in param_space_list])
+        volumes = np.array([self.prescan_parser.compute_effective_volume(space) for space in param_space_list])
 
         # Retrieve total volume of all param spaces
         total_volume = volumes.sum()
