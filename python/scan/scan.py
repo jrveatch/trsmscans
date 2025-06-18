0068e081b5cd50c5c050b60fd7be44df8e935f0f
Jason Veatch <jason.veatch@cern.ch>
2025-06-18 19:00:49 +0200

    Fix spelling

--- python/scan/scan.py
+++ python/scan/scan.py
@@ -462,7 +462,7 @@ class Scan:
     
     def get_param_spaces(self, param_space: 'ParamSpace') -> List[ParamSpace]:
 
-        # Intialize Lists to hold the current param spaces and the final param spaces
+        # Initialize Lists to hold the current param spaces and the final param spaces
         current_param_space_list = [param_space]
         final_param_space_list =[]
 
@@ -562,7 +562,7 @@ class Scan:
         # Assign rounded points to the points_per_optimizer_array
         points_per_optimizer_array = points_array
 
-        # Minimum points assigment
+        # Minimum points assignment
         min_points = min(num_points/10,20)
         self.logger.debug(f"Minimum points per zoom optimizer: {min_points}")
 
