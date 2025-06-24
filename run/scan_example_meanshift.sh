2d0ea9418c0d4e64561b70c1512c44c332446833
Jason Veatch <jveatch@cern.ch>
2025-06-24 14:31:22 +0200

    Fix argument requirements for zoom vs meanshift (#433)
    Co-authored-by: Jason Veatch <jason.veatch@cern.ch>

--- run/scan_example_meanshift.sh
+++ run/scan_example_meanshift.sh
@@ -1,2 +1,2 @@
 
-run_process.py --mode "scan" -X 1001 -S 301 -m "TRSMBroken" -d "SbbHtautau" -i 5 -s "meanshift"
+run_process.py --mode "scan" -X 1001 -S 301 -m "TRSMBroken" -d "SbbHtautau" -t 5 -s "meanshift" -o
